b2366b7672be3a062ff66a5bf7f58d612bf82f2f
Phil Wells <phil.wells@nytimes.com>
2018-07-02 14:28:57 -0400

    port test site

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -74,7 +74,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://localhost:8000/osx/www/index.html',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
