5e395d87cda64f671b48294481770c4daaf81d02
Andrii Lazebnyi <lazebniy.a@gmail.com>
2018-07-07 22:58:43 +0300

    wdio-concise-reporter: initial commit

---
+++ packages/wdio-concise-reporter/README.md
@@ -0,0 +1,18 @@
+WDIO Concise Reporter
+=====================
+
+> A WebdriverIO plugin to report in concise style.
+
+## Configuration
+
+The following code shows the default wdio test runner configuration. Just add `'concise'` as a reporter
+to the array.
+
+```js
+// wdio.conf.js
+module.exports = {
+  // ...
+  reporters: ['dot', 'concise'],
+  // ...
+};
+```
---
+++ packages/wdio-concise-reporter/src/index.js
@@ -0,0 +1,178 @@
+import WDIOReporter from 'wdio-reporter'
+import chalk from 'chalk'
+
+export default class ConciseReporter extends WDIOReporter {
+    constructor (options) {
+        /**
+         * make concise reporter to write to output stream by default
+         */
+        options = Object.assign(options, { stdout: true })
+        super(options)
+
+        // Keep track of the order that suites were called
+        this.suiteUids = []
+
+        this.suites = []
+        this.suiteIndents = {}
+        this.defaultTestIndent = '   '
+        this.stateCounts = {
+            passed : 0,
+            failed : 0,
+            skipped : 0
+        }
+
+        this.chalk = chalk
+    }
+
+    onSuiteStart (suite) {
+        this.suiteUids.push(suite.uid)
+        this.suiteIndents[suite.uid] = ++this.indents
+    }
+
+    onSuiteEnd (suite) {
+        this.indents--
+        this.suites.push(suite)
+    }
+
+    onTestPass () {
+        this.stateCounts.passed++
+    }
+
+    onTestFail () {
+        this.stateCounts.failed++
+    }
+
+    onTestSkip () {
+        this.stateCounts.skipped++
+    }
+
+    onRunnerEnd (runner) {
+        this.printReport(runner)
+    }
+
+    /**
+     * Print the report to the screen
+     */
+    printReport(runner) {
+        const header = this.chalk.yellow('========= Your concise report ==========')
+
+        const output = [
+            ...this.getHeaderDisplay(runner),
+            this.getCountDisplay(),
+            ...this.getFailureDisplay()
+        ]
+
+        // Output the results
+        this.write(`${header}\n${output.join(`\n`)}\n`)
+    }
+
+    /**
+     * Get the header display for the report
+     * @param  {Object} runner Runner data
+     * @return {Array}         Header data
+     */
+    getHeaderDisplay(runner) {
+        const combo = this.getEnviromentCombo(runner.capabilities).trim()
+
+        // Spec file name and enviroment information
+        const output = [
+            `${combo}`
+        ]
+
+        return output
+    }
+
+    /**
+     * Get the display for failing tests
+     * @return {String} Count display
+     */
+    getCountDisplay () {
+        const failedTestsCount = this.stateCounts.failed
+
+        if(failedTestsCount > 0) {
+            return `Test${failedTestsCount > 1 ? 's' : ''} failed (${failedTestsCount}):`
+        } else {
+            return 'All went well !!'
+        }
+    }
+
+    /**
+     * Get display for failed tests, e.g. stack trace
+     * @return {Array} Stack trace output
+     */
+    getFailureDisplay () {
+        const output = []
+        const suites = this.getOrderedSuites()
+
+        for (const suite of suites) {
+            for (const test of suite.tests) {
+                if (test.state !== 'failed') {
+                    continue
+                }
+
+                // If we get here then there is a failed test
+                output.push(
+                    `    Fail : ${this.chalk.red(test.title)}`,
+                    `        ${test.error.type} : ${this.chalk.yellow(test.error.message)}`
+                )
+            }
+        }
+
+        return output
+    }
+
+    /**
+     * Get suites in the order they were called
+     * @return {Array} Ordered suites
+     */
+    getOrderedSuites () {
+        if (this.orderedSuites) {
+            return this.orderedSuites
+        }
+
+        this.orderedSuites = []
+        for (const uid of this.suiteUids) {
+            for (const suite of this.suites) {
+                if (suite.uid !== uid) {
+                    continue
+                }
+
+                this.orderedSuites.push(suite)
+            }
+        }
+
+        return this.orderedSuites
+    }
+
+    /**
+     * Get information about the enviroment
+     * @param  {Object}  caps    Enviroment details
+     * @param  {Boolean} verbose
+     * @return {String}          Enviroment string
+     */
+    getEnviromentCombo (caps, verbose = true) {
+        const resolution = caps.resolution || caps.screenResolution
+        const device = caps.deviceName
+        const browser = caps.browserName || caps.browser
+        const version = caps.version || caps.platformVersion || caps.browser_version
+        const platform = caps.os ? (caps.os + ' ' + caps.os_version) : (caps.platform || caps.platformName)
+
+        // Mobile capabilities
+        if (device) {
+            const program = (caps.app || '').replace('sauce-storage:', '') || caps.browserName
+            const executing = program ? `executing ${program}` : ''
+
+            if (!verbose) {
+                return `${device} ${platform} ${version}`
+            }
+
+            return `${device} on ${platform} ${version} ${executing}`.trim()
+        }
+
+        if (!verbose) {
+            return (browser + ' ' + (version || '') + ' ' + (platform || '')).trim()
+        }
+
+        return browser + (version ? ` (v${version})` : '') + (platform ? ` on ${platform}` : '') + (resolution ? ` ${resolution}` : '')
+    }
+}
