1f14f7e5b767bc7cc92bc2c3776c0c18945624c0
Andrii Lazebnyi <lazebniy.a@gmail.com>
2018-07-08 23:41:13 +0300

    wdio-concise-reporter: finish reporter

--- packages/wdio-concise-reporter/src/index.js
+++ packages/wdio-concise-reporter/src/index.js
@@ -4,12 +4,12 @@ import chalk from 'chalk'
 export default class ConciseReporter extends WDIOReporter {
     constructor (options) {
         /**
-         * make concise reporter to write to output stream by default
+         * make Concise reporter to write to output stream by default
          */
         options = Object.assign(options, { stdout: true })
         super(options)
 
-        // Keep track of the order that suites were called
+        // keep track of the order that suites were called
         this.suiteUids = []
 
         this.suites = []
@@ -38,6 +38,7 @@ export default class ConciseReporter extends WDIOReporter {
 
     /**
      * Print the Concise report to the screen
+     * @param  {Object} runner Wdio runner
      */
     printReport(runner) {
         const header = this.chalk.yellow('========= Your concise report ==========')
@@ -48,7 +49,6 @@ export default class ConciseReporter extends WDIOReporter {
             ...this.getFailureDisplay()
         ]
 
-        // Output the results
         this.write(`${header}\n${output.join(`\n`)}\n`)
     }
 
@@ -73,7 +73,6 @@ export default class ConciseReporter extends WDIOReporter {
 
         this.getOrderedSuites().map(suite => suite.tests.map(test => {
             if (test.state === 'failed') {
-                // If we get here then there is a failed test
                 output.push(
                     `  Fail : ${this.chalk.red(test.title)}`,
                     `    ${test.error.type} : ${this.chalk.yellow(test.error.message)}`
@@ -89,20 +88,13 @@ export default class ConciseReporter extends WDIOReporter {
      * @return {Array} Ordered suites
      */
     getOrderedSuites () {
-        if (this.orderedSuites) {
-            return this.orderedSuites
-        }
-
         this.orderedSuites = []
-        for (const uid of this.suiteUids) {
-            for (const suite of this.suites) {
-                if (suite.uid !== uid) {
-                    continue
-                }
 
+        this.suiteUids.map(uid => this.suites.map(suite => {
+            if (suite.uid === uid) {
                 this.orderedSuites.push(suite)
             }
-        }
+        }))
 
         return this.orderedSuites
     }
@@ -113,29 +105,22 @@ export default class ConciseReporter extends WDIOReporter {
      * @param  {Boolean} verbose
      * @return {String}          Enviroment string
      */
-    getEnviromentCombo (caps, verbose = true) {
-        const resolution = caps.resolution || caps.screenResolution
+    getEnviromentCombo (caps) {
         const device = caps.deviceName
         const browser = caps.browserName || caps.browser
         const version = caps.version || caps.platformVersion || caps.browser_version
         const platform = caps.os ? (caps.os + ' ' + caps.os_version) : (caps.platform || caps.platformName)
 
-        // Mobile capabilities
+        // mobile capabilities
         if (device) {
             const program = (caps.app || '').replace('sauce-storage:', '') || caps.browserName
             const executing = program ? `executing ${program}` : ''
 
-            if (!verbose) {
-                return `${device} ${platform} ${version}`
-            }
-
-            return `${device} on ${platform} ${version} ${executing}`.trim()
+            return `${device} on ${platform} ${version} ${executing}`
         }
 
-        if (!verbose) {
-            return (browser + ' ' + (version || '') + ' ' + (platform || '')).trim()
-        }
-
-        return browser + (version ? ` (v${version})` : '') + (platform ? ` on ${platform}` : '') + (resolution ? ` ${resolution}` : '')
+        return browser
+            + (version ? ` (v${version})` : '')
+            + (platform ? ` on ${platform}` : '')
     }
 }
